cc15d5db2f41f737d315feb8dbce3286565d1863
mohammed dhedy <mhmddhedy1234@gmail.com>
2024-12-22 12:58:23 +0200

    update the style of the result and change the font family for the body

--- calculator/src/Result/Result.component.tsx
+++ calculator/src/Result/Result.component.tsx
@@ -1,10 +1,13 @@
+import "./Result.style.css";
 interface Iprops {
   finalResult: string;
 }
 export default function Result(props: Iprops): JSX.Element {
   return (
-    <div>
-      <h2>{props.finalResult=="0"?"":props.finalResult}</h2>
+    <div className="result">
+      <h2>
+        {props.finalResult == "0" ? "" : props.finalResult}
+      </h2>
     </div>
   );
 }
